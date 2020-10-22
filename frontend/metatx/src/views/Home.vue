b954e8b18854c868217eae03378d2942688f1687
hp <hp_ctt@hotmail.com>
2020-10-22 16:29:43 +0100

    wip contract

--- frontend/metatx/src/views/Home.vue
+++ frontend/metatx/src/views/Home.vue
@@ -1,29 +1,52 @@
 <template>
   <div class="home">
- 
     <section>
-            <input type="text" id="address" v-model="address" />
-
+      <div> 
+        Metamask loaded address: 
+        <input type="text" id="address" v-model="address" style="width:100%"/>
+      </div>
+      <br/>
+      <div>
+        <span>Nonce: {{nonce}}</span>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
+import Web3 from "web3";
+import MetaTransactions from "../assets/MetaTransactions.json";
+
 export default {
   name: "Home",
   components: {},
   data() {
     return {
-      address:"",
+      address: "",
+      nonce: 0,
+      metacontract: "",
     };
   },
-  mounted(){
-    this.init()
+  async mounted() {
+    await this.init();
   },
-   methods:{
-    init(){
-      
-    }
+  methods: {
+    async init() {
+      window.ethereum.enable().catch((error) => {
+        console.log("error ethereum", error);
+      });
+      this.web3 = new Web3(window.ethereum);
+
+      // this.contract = new this.web3.eth.contract(MetaTransactions)
+
+
+      this.web3.eth.getAccounts().then((_accounts) => {
+        this.address = _accounts[0];
+      });
+  
+      // this.nonce = await contract.methods.getNonce(this.address).call();
+
+    },
   },
 };
 </script>
